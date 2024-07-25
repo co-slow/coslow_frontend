2429bc39ebbde90f67a793dff2f41cdd9d7f99f7
No4h <no4h1209@gmail.com>
2024-07-25 17:00:25 +0900

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import AddExplain from './Components/mainpage/AddExplain'
+import Footer from './Components/mainpage/Footer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AddExplain />
+    <Footer />
+  </React.StrictMode>,
+)
