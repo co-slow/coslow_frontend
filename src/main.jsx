4a0c9f36c0e3a5d901fc3ba49fe820382d42f47b
heeyoung00 <ils09019@naver.com>
2024-07-25 14:09:28 +0900

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Coslow_banner from './Components/mainpage/Coslow_banner'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Coslow_banner />
+  </React.StrictMode>,
+)
