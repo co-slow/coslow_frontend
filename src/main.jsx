3e68f499b4a7e863619c4b1460716e4bfecb6a0c
heeyoung00 <ils09019@naver.com>
2024-07-26 14:35:08 +0900

    합침

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import Coslow_banner from './Components/mainpage/Coslow_banner'
 import './index.css'
 import Challenge_main from './Components/challenge/Challenge_main';
 import Coslow_main from './Components/mainpage/Coslow_main';
+import AddExplain from './Components/mainpage/AddExplain'
+import Footer from './Components/mainpage/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<Coslow_banner />}/>
-        <Route path='/afterlogin' element={<Coslow_main />}/>
+        <Route path='/' element={<div><Coslow_banner /><AddExplain /><Footer /></div>}/>
+        <Route path='/afterlogin' element={<div><Coslow_main /><AddExplain /><Footer /></div>}/>
         <Route path='/challenge' element={<Challenge_main/>}/>
       </Routes>
     </Router>
